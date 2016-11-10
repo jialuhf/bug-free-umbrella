6dbe9c26d1252fad303922e764ff99096b0ff276
jialuhf <jialuhf@gmail.com>
2016-11-10 16:40:18 -0500

    Add files via upload

---
+++ cachematrix.R
@@ -0,0 +1,33 @@
+## The following functions work together to create a square invertible matrix
+## and make the inverse of the matrix available in the cache environment
+
+## makeCacheMatrix creates and returns a list of functions
+## used by cacheSolve to get or set the inverted matrix in cache
+
+makeCacheMatrix <- function(x = matrix()) {
+        m <- NULL
+        set <- function(y) {
+                x <<- y
+                m <<- NULL
+        }
+        get <- function() x
+        setinverse <- function(solve) m <<- solve
+        getinverse <- function() m
+        list(set = set, get = get,
+             setinverse = setinverse,
+             getinverse = getinverse)
+}
+
+## cacheSolve calcluates the inverse of the matrix created in makeCacheMatrix
+
+cacheSolve <- function(x, ...) {
+        m <- x$getinverse()
+        if(!is.null(m)) {
+                message("getting cached data")
+                return(m)
+        }
+        data <- x$get()
+        m <- solve(data, ...)
+        x$setinverse(m)
+        m
+}
